78a5a4a8112df1a56f46f7343b5047b912f3f5b1
mariusgaida1 <mariusgaida1@gmail.com>
2024-05-09 16:32:49 +0300

    add Card and Deck classes

--- war.py
+++ war.py
@@ -1,5 +1,44 @@
+import csv
+import random
+
+class Card:
+    def __init__(self, unicode, card, rank):
+        self.unicode = unicode
+        self.card = card
+        self.rank = rank
+    
+    def __str__(self):
+        return f"{self.unicode} {self.card} ({self.rank})"
+    
+class Deck:
+    def __init__(self, file_csv):
+        self.cards = []
+        self.load_cards(file_csv)
+        self.shuffle_deck()
+    
+    def load_cards(self, file_csv):
+        with open(file_csv, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                self.cards.append(Card(row['Unicode'], row['Card'], row['Rank']))
+    
+    def shuffle_deck(self):
+        random.shuffle(self.cards)
+
+    def __str__(self):
+        return '\n'.join(map(str, self.cards))
+
+
+class Player:
+    ...
+
+class Game:
+    ...
+
 def main():
-    with 
+    deck = Deck('cards.csv')
+    print(deck)
+
 
 if __name__ == "__main__":
     main()
