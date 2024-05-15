3b075aaaa79a3f5778ebf74d378f76bc9dc8aef5
mariusgaida1 <mariusgaida1@gmail.com>
2024-05-15 22:11:12 +0300

    Add docstrings and comments to code

--- war.py
+++ war.py
@@ -3,6 +3,14 @@ import random
 import sys
 
 class Card:
+    """
+    Represents a playing card.
+
+    Attributes:
+        unicode (str): Unicode symbol of the card.
+        card (str): Name of the card.
+        rank (str): Rank of the card.
+    """
     def __init__(self, unicode, card, rank):
         self.unicode = unicode
         self.card = card
@@ -12,21 +20,42 @@ class Card:
         return f"{self.unicode} {self.card} ({self.rank})"
     
 class Deck:
+    """
+    Represents a deck of cards.
+
+    Attributes:
+        file_csv (str): CSV file containing card information.
+        cards (list): List of Card objects representing the deck.
+        list1 (list): First players deck.
+        list2 (list): Second players deck.
+    """
     def __init__(self, file_csv):
         self.cards = []
         self.load_cards(file_csv)
         self.shuffle_deck()
     
     def load_cards(self, file_csv):
+        """
+        Loads cards from a CSV file into the deck.
+        """
         with open(file_csv, 'r') as file:
             reader = csv.DictReader(file)
             for row in reader:
                 self.cards.append(Card(row['Unicode'], row['Card'], row['Rank']))
     
     def shuffle_deck(self):
+        """
+        Shuffles the deck of cards.
+        """
         random.shuffle(self.cards)
     
     def deck_split(self):
+        """
+        Splits the deck into two halves.
+
+        Returns:
+            tuple: Two lists representing each split of the deck.
+        """
         num_of_cards_for_each = len(self.cards) // 2
         self.list1 = self.cards[:num_of_cards_for_each]
         self.list2 = self.cards[num_of_cards_for_each:]
@@ -37,17 +66,37 @@ class Deck:
 
 
 class Player:
+    """
+    Represents a player in the card game.
+
+    Attributes:
+        players_deck (list): List of Card objects representing the player's deck.
+        has_lost (bool): Indicates if the player has lost the game.
+    """
     def __init__(self):
         self.players_deck = []
         self.has_lost = False
 
 class Game:
+    """
+    Represents the card game.
+
+    Attributes:
+        player1 (Player): First player.
+        player2 (Player): Second player.
+    """
     def __init__(self, deck):
         self.player1 = Player()
         self.player2 = Player()
         self.player1.players_deck, self.player2.players_deck = deck.deck_split()
     
     def game_move(self):
+        """
+        Determines the outcome of a move in the game.
+
+        Returns:
+            str: Outcome of the move - 'play1', 'play2', or 'war'.
+        """
         card1 = int(self.player1.players_deck[0].rank)
         card2 = int(self.player2.players_deck[0].rank)
         if card1 > card2:
